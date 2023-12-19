1dcae2755031f64ca56eecb012c6a459dd1dd3de
swos <szymon.wos1319@gmail.com>
2023-12-19 11:46:06 +0100

    Add operate function that chooses operation
    The function should choose correct operation based on variable and give result of it

--- script.js
+++ script.js
@@ -14,3 +14,22 @@ function divide(dividend, divisor){
     if(divisor == 0) return 'ERROR';
     return dividend/divisor;
 }
+
+function operate(operator, numberOne, numberTwo){
+    let result;
+    switch (operator){
+        case '+':
+            result = add(numberOne, numberTwo);
+            break;
+        case '-':
+            result = substract(numberOne, numberTwo);
+            break;
+        case 'x':
+            result = multiply(numberOne, numberTwo);
+            break;
+        case '/':
+            result = divide(numberOne, numberTwo);
+            break;
+    }
+    return result;
+}
